44e9d9b46f0efdce15c1c8790283befc843ed9e8
zl90 <mb541@protonmail.com>
2025-08-03 20:39:56 +1000

    Update directory structure

---
+++ include/order-book/OrderBook.h
@@ -0,0 +1,16 @@
+//
+// Created by mint on 3/08/25.
+//
+
+#ifndef ORDERBOOK_H
+#define ORDERBOOK_H
+
+
+
+class OrderBook {
+
+};
+
+
+
+#endif //ORDERBOOK_H
---
+++ src/order-book/OrderBook.cpp
@@ -0,0 +1,2 @@
+
+#include "../../include/order-book/OrderBook.h"
