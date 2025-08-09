93fbfa144859a5d4a6d8b7ca14031a94a150ed58
zl90 <mb541@protonmail.com>
2025-08-09 22:22:57 +1000

    Implement limit order fill

--- src/order-book/OrderBook.cpp
+++ src/order-book/OrderBook.cpp
@@ -90,24 +90,63 @@ auto OrderBook::SubmitOrder(const Order &order) -> std::vector<Trade> {
 auto OrderBook::HandleLimitOrder(const Order &order) -> std::vector<Trade> {
     auto [type, user_id, level, quantity, side, order_id] = order;
     if (side == Side::BUY) {
-        if (_asks.empty() || GetBestAsk() > level) {
+        std::optional<PriceType> best_ask = !_asks.empty() ? GetBestAsk() : std::nullopt;
+        if (_asks.empty() || best_ask.value() > level.value()) {
             const Trade accept_trade{
                 TradeType::ACCEPTED, user_id, std::nullopt, order_id, std::nullopt, level, quantity, side
             };
             InsertRestingLimitOrder(order);
             return {accept_trade};
         } else {
-            return {};
+            auto &mm_queue = _asks.find(level.value())->second.market_maker_queue;
+            auto &pt_queue = _asks.find(level.value())->second.participants_queue;
+            const auto &best_offer = mm_queue.size() > 0 ? mm_queue.begin() : pt_queue.begin();
+            const bool is_best_offer_in_mm_queue = mm_queue.size() > 0;
+
+            const Trade fill_trade = {
+                TradeType::FILL, user_id, best_offer->user_id, order_id, best_offer->order_id, level.value(), quantity,
+                side, std::nullopt, FilledSide::MAKER
+            };
+
+            if (is_best_offer_in_mm_queue) {
+                mm_queue.erase(best_offer);
+                mm_queue.erase(std::remove(mm_queue.begin(), mm_queue.end(), best_offer), mm_queue.end());
+            } else {
+                pt_queue.erase(best_offer);
+                pt_queue.erase(std::remove(pt_queue.begin(), pt_queue.end(), best_offer), pt_queue.end());
+            }
+
+            return {fill_trade};
         }
     } else if (side == Side::SELL) {
-        if (_bids.empty() || GetBestBid() < level) {
+        std::optional<PriceType> best_bid = !_bids.empty() ? GetBestBid() : std::nullopt;
+
+        if (_bids.empty() || best_bid.value() < level.value()) {
             const Trade accept_trade{
                 TradeType::ACCEPTED, user_id, std::nullopt, order_id, std::nullopt, level, quantity, side
             };
             InsertRestingLimitOrder(order);
             return {accept_trade};
         } else {
-            return {};
+            auto &mm_queue = _bids.find(level.value())->second.market_maker_queue;
+            auto &pt_queue = _bids.find(level.value())->second.participants_queue;
+            const auto &best_bid = mm_queue.size() > 0 ? mm_queue.begin() : pt_queue.begin();
+            const bool is_best_bid_in_mm_queue = mm_queue.size() > 0;
+
+            const Trade fill_trade = {
+                TradeType::FILL, user_id, best_bid->user_id, order_id, best_bid->order_id, level.value(), quantity,
+                side, std::nullopt, FilledSide::MAKER
+            };
+
+            if (is_best_bid_in_mm_queue) {
+                mm_queue.erase(best_bid);
+                mm_queue.erase(std::remove(mm_queue.begin(), mm_queue.end(), best_bid), mm_queue.end());
+            } else {
+                pt_queue.erase(best_bid);
+                pt_queue.erase(std::remove(pt_queue.begin(), pt_queue.end(), best_bid), pt_queue.end());
+            }
+
+            return {fill_trade};
         }
     } else {
         throw std::runtime_error("Unknown side in HandleLimitOrder");
